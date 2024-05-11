b33b34a720e7ca02233b44c8848470001ff4e673
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-11 20:59:41 +0400

    add: wrapp application with redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {BrowserRouter as Router} from "react-router-dom"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>      
-    <App />
-    </Router>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
