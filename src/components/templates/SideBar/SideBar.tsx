8c2bba5a49c88c8c2fcb32757626b194c3b2f318
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-16 22:39:40 +0400

    add: toggleSideBar function and change some styles

--- src/components/templates/SideBar/SideBar.tsx
+++ src/components/templates/SideBar/SideBar.tsx
@@ -4,6 +4,7 @@ import Button from "../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
 import { SignOutAction } from "../../../features/userSlice";
+import { toggleSideBar } from "../../../features/sideBarSlice";
 import AuthButtons from "../../../shared/AuthButtons";
 
 export const SideBar = () => {
@@ -12,14 +13,24 @@ export const SideBar = () => {
   const onSignOut = () => {
     dispatch(SignOutAction());
   };
+
+  const handleToggleSideBar = () => {
+    dispatch(toggleSideBar());
+  };
   const isLoggedIn = useSelector(
     (state: RootState) => state.userReducer.isLoggedIn
   );
 
+  const isSideBarOpen = useSelector(
+    (state: RootState) => state.sideBarReducer.isSideBarOpen
+  );
+
   return (
-    <nav className="h-screen bg-black bg-opacity-10 px-20 py-5">
-      <div className="h-full flex flex-col justify-between items-center">
-        <ul className="flex flex-col items-center">
+    <nav
+      className={`${isSideBarOpen ? "h-screen p-6 fixed flex justify-between gap-20  bg-gradient-to-tl from-blue-600 to-purple-600 bg-no-repeat" : "hidden"} `}
+    >
+      <div className="h-full flex flex-col justify-between items-center px-6">
+        <ul className=" flex flex-col items-center">
           <h1 className="text-4xl font-bold pb-6">Movies</h1>
           {navigationConfig.map((item) => {
             const Icon = item.icon;
@@ -53,6 +64,11 @@ export const SideBar = () => {
             </div>
           )}
         </ul>
+      </div>
+      <div>
+        <button onClick={handleToggleSideBar} className="text-3xl font-bold">
+          x
+        </button>
       </div>
     </nav>
   );
